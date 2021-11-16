b5b85c64d2e53801c8377af8e378d6e7a215440c
Sumanth Kumar <sumanthkumarkotagudem@gmail.com>
2021-11-17 00:04:39 +0530

    real time chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,6 +5,7 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import db from "../Firebase";
 import "./Chat.css"
 import Message from './Message';
+import ChatInput from './ChatInput';
 
 function Chat() {
     const { roomId } = useParams(); 
@@ -52,6 +53,7 @@ function Chat() {
                     />
                 )}
             </div>
+            <ChatInput channelName={roomDetails?.name} channelId={roomId} />
         </div>
     )
 }
